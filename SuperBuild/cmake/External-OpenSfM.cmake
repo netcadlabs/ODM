0d53307ac755a0e6dd0aadf8f692f17c6484fac7
omerkalkan <omer.kalkan@netcad.com>
2022-01-14 20:37:16 +0300

    github action ayarlari

--- SuperBuild/cmake/External-OpenSfM.cmake
+++ SuperBuild/cmake/External-OpenSfM.cmake
@@ -18,7 +18,7 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/OpenDroneMap/OpenSfM/
+  GIT_REPOSITORY    https://github.com/netcadlabs/OpenSfM/
   GIT_TAG           270
   #--Update/Patch step----------
   UPDATE_COMMAND    git submodule update --init --recursive
